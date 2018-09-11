68171978272c76707cb673689952d8ea5086016e
sofia <sruiz13@lion.lmu.edu>
2018-09-10 19:22:15 -0700

    13 tests passing

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,4 +1,4 @@
-function change (amount) {
+exports.change = (amount) => {
   if (amount === 0) {
     return [0, 0, 0, 0];
   }
@@ -22,45 +22,56 @@ function change (amount) {
   const pennies = newAmount;
 
   return [quarters, dimes, nickels, pennies];
-}
-
-/* console.log(change(97));
-console.log(change(8));
-console.log(change(250));
-console.log(change(144));
-console.log(change(97));
-console.log(change(-4));
-console.log(change(0));
-console.log(change(1000000000)); */
+};
 
 
-function stripQuotes(input) {
-  const result = input.replace(/\W/g, '');
+exports.stripQuotes = input => input.replace(/['"]/g, '');
+
+
+exports.scramble = input => input.split('').sort(() => 0.5 - Math.random()).join('');
+
+
+exports.powers = (base, limit, callback) => {
+  for (let i = 1; i <= limit; i *= base) {
+    callback(i);
+  }
+};
+
+
+exports.powersGenerator = function* g(base, limit) {
+  for (let x = 1; x <= limit; x *= base) {
+    yield x;
+  }
+};
+
+
+exports.say = (word) => {
+  if (!word) {
+    return '';
+  }
+  return nextword => (!nextword ? word : exports.say(`${word} ${nextword}`));
+};
+
+exports.interleave = (a, ...b) => {
+  const resLen = a.length + b.length
+  const result = [];
+  for (let i = 0; i <= a.length; i += 1) {
+    result.push(a[i]);
+    result.push(b[i]);
+    if (result.length > resLen) {
+      result.pop();
+    }
+  }
 
   return result;
-}
+};
 
-/* console.log(stripQuotes(`a\"\""""\'to\"\"'\"\"''z`));
-console.log(stripQuotes(''));
-console.log(stripQuotes("Hello, world"));
-console.log(stripQuotes('"\''));
-console.log(stripQuotes('a"""\'\'"z')); */
-
-
-function scramble(input) {
-  const scrambled = input.split('').sort(() => 0.5 - Math.random()).join('');
-
-  return scrambled;
-}
-
-// console.log(scramble('Hello, world'));
-
-function powers(base, limit) {
-  for (let i = 1; i <= limit; i++) {
-    const power = base ** i;
-
-    return power;
-  }
-}
-
-// console.log(powers(2, 70, p => console.log(p)));
+exports.randomName = (gen, reg) => {
+  const rp = require('request-promise');
+  const gender = gen;
+  const region = reg;
+  rp({
+    uri: 'http://uinames.com/api',
+    qs: { gender, region },
+  }).then(person => person);
+};
