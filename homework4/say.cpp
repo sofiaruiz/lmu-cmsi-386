a1308a6e2c35417ebefd566394f71c95b9a29d2e
sofia <sruiz13@lion.lmu.edu>
2018-11-20 11:49:55 -0800

    homework 4 attempt

---
+++ homework4/say.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    std::string operator();
+    str1 = "Test string: ";   // c-string
+    str2 = 'x';               // single character
+    str3 = str1 + str2;       // string
+
+    std::cout << str3  << '\n';
+    return 0;
+
+}
